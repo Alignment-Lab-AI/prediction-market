2c747cfad36405aa8c560ad15ce9a871809e561d
Pratik <pratikasr@gmail.com>
2024-09-17 21:37:43 +0530

    updating config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://95.216.154.108:1317/:path*',
-        },
-        {
-          source: '/rpc/:path*',
-          destination: 'http://95.216.154.108:26657/:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
